aa04f2ae290033afd57b79c36a4755108ee4ac7e
Princess <sonali.patel.pkg@gmail.com>
2022-03-04 18:17:18 -0600

    ui updated

---
+++ src/EventPage/EventCard.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+function EventCard() {
+    return (
+      <div>
+    
+    <hr />
+
+    <div className='eventCard'>
+      <h2>Event name</h2>
+      <h4>Date and time of event</h4>
+      <h3>Venue</h3>
+      <h3>Number invited</h3>
+    </div>
+
+   
+
+
+      </div>
+    )
+}
+
+export default EventCard
---
+++ src/GlobalAttributes/Header.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+import flowers from './flower.png';
+
+function Header() {
+    return (
+      
+      <div>
+
+      <div className='header'>
+          
+    <div className='couple-header'>
+       <h1>Hassan & Sanjay</h1>
+    </div>
+       
+       <h1>3.11.2022</h1>
+
+      </div>
+
+      <img 
+        src= {flowers}
+        alt="white flowers" />
+        
+      </div>
+    )
+}
+
+export default Header
